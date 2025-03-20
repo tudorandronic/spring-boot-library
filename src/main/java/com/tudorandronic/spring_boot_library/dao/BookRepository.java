336585586fe2131248b05ef21943791713340e40
tudorandronic <save_tudor@yahoo.com>
2025-03-20 10:05:43 +0200

    Added findBookByTitle functionality

--- src/main/java/com/tudorandronic/spring_boot_library/dao/BookRepository.java
+++ src/main/java/com/tudorandronic/spring_boot_library/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package com.tudorandronic.spring_boot_library.dao;
 
 import com.tudorandronic.spring_boot_library.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    Page<Book> findBookByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
