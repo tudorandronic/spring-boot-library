6c741526cebbb9c9f46818ce23d8dba2e272779a
tudorandronic <save_tudor@yahoo.com>
2025-04-15 17:08:05 +0300

    BookPageFunctionality
    Added the edpoints for the BookPage

--- src/main/java/com/tudorandronic/spring_boot_library/dao/BookRepository.java
+++ src/main/java/com/tudorandronic/spring_boot_library/dao/BookRepository.java
@@ -4,6 +4,7 @@ import com.tudorandronic.spring_boot_library.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
@@ -11,4 +12,6 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     Page<Book> findBookByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 
     Page<Book> findBookByCategoryContaining(@RequestParam("category") String category, Pageable pageable);
+
+    Page<Book> findBookById(@RequestParam("id") Long id, Pageable pageable);
 }
